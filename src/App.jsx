ff882b695dd4dfe14f12f2c141da2a6b42afbd5c
PelekhIgor <igorpelekh0112@gmail.com>
2023-02-28 16:42:53 +0200

    add form

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import LoginPage from "./login-page";
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import { c } from 'react-router-dom'
+
+
+
+
+
+export default function App(){
+  return (
+      <>
+        <LoginPage></LoginPage>
+      </>
+  )
+}
