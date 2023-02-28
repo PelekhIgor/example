8cb745b7ed6b7f9ba25c1b37afc8be96e50eaa21
PelekhIgor <igorpelekh0112@gmail.com>
2023-02-28 16:47:00 +0200

    add

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter,
+    RouterProvider,
+    Navigate,
+} from "react-router-dom"
+import LoginPage from "./login-page";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Navigate replace to="/login"/>
+    },
+    {
+        path: "/login",
+        element: <LoginPage/>
+    },
+    ])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-          <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
+reportWebVitals()
